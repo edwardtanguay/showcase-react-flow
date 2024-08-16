065f17eaf17e2786af59d1e65e4c505a11378a26
Edward Tanguay (windows) <edward@tanguay.info>
2024-08-16 12:25:22 +0200

    republish

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
-import { ReactFlow, Node } from '@xyflow/react';
+import { useCallback } from 'react';
+import {
+  ReactFlow,
+  Background,
+  Controls,
+  MiniMap,
+  addEdge,
+  useNodesState,
+  useEdgesState,
+  type OnConnect,
+} from '@xyflow/react';
+
 import '@xyflow/react/dist/style.css';
 
-export default function App() {
+import { initialNodes, nodeTypes } from './nodes';
+import { initialEdges, edgeTypes } from './edges';
 
-  const nodes: Node[] = [{
-    id: '1',
-    data: {
-      label: 'Information'
-    },
-    position: { x: 0, y: 0 }
-  }];
+export default function App() {
+  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect: OnConnect = useCallback(
+    (connection) => setEdges((edges) => addEdge(connection, edges)),
+    [setEdges] 
+  );
 
   return (
-    <ReactFlow nodes={nodes} />
+    <ReactFlow
+      nodes={nodes}
+      nodeTypes={nodeTypes}
+      onNodesChange={onNodesChange}
+      edges={edges}
+      edgeTypes={edgeTypes}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      fitView={true}
+    >
+      <Background />
+      <MiniMap />
+      <Controls />
+    </ReactFlow>
   );
 }
