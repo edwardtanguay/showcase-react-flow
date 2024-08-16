b687fcb38c836ec2879a07e572b95cf60ee26062
Edward Tanguay (windows) <edward@tanguay.info>
2024-08-16 14:18:54 +0200

    should move

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,89 @@
-import { ReactFlow, Node, Edge, Background, Controls } from "@xyflow/react";
+import {
+	ReactFlow,
+	Node,
+	Edge,
+	Background,
+	Controls,
+	useNodesState,
+	useEdgesState,
+} from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 
-export default function App() {
-	const nodes: Node[] = [
-		{
-			id: "1",
-			data: {
-				label: "Information",
-			},
-			position: { x: 0, y: 0 },
-			hidden: true,
+const initialNodes: Node[] = [
+	{
+		id: "1",
+		data: {
+			label: "Information",
 		},
-		{
-			id: "2",
-			data: {
-				label: "Welcome",
-			},
-			position: { x: 100, y: 50 },
-			selected: true,
+		position: { x: 0, y: 0 },
+		hidden: true,
+	},
+	{
+		id: "2",
+		data: {
+			label: "Welcome",
 		},
-		{
-			id: "3",
-			data: {
-				label: "Records",
-			},
-			position: { x: 260, y: 150 },
-			selected: false,
-			selectable: true,
-			draggable: false,
-			resizing: true,
-			width: 130,
-			height: 130,
+		position: { x: 100, y: 50 },
+		selected: true,
+	},
+	{
+		id: "3",
+		data: {
+			label: "Records",
 		},
-		{
-			id: "4",
-			data: {
-				label: "Overview",
-			},
-			position: { x: 390, y: 350 },
-			selected: false,
-			selectable: true,
-			draggable: false,
-			resizing: true,
-			width: 130,
-			height: 130,
+		position: { x: 260, y: 150 },
+		selected: false,
+		selectable: true,
+		draggable: false,
+		resizing: true,
+		width: 130,
+		height: 130,
+	},
+	{
+		id: "4",
+		data: {
+			label: "Overview",
 		},
-	];
+		position: { x: 390, y: 350 },
+		selected: false,
+		selectable: true,
+		draggable: false,
+		resizing: true,
+		width: 130,
+		height: 130,
+	},
+];
 
-	const edges: Edge[] = [
-		{
-			id: "2-3",
-			source: "2",
-			target: "3",
-			style: { stroke: "black" },
-			animated: true,
-		},
-		{
-			id: "3-4",
-			source: "3",
-			target: "4",
-			style: { stroke: "black" },
-		},
-	];
+const initialEdges: Edge[] = [
+	{
+		id: "2-3",
+		source: "2",
+		target: "3",
+		style: { stroke: "black" },
+		animated: true,
+	},
+	{
+		id: "3-4",
+		source: "3",
+		target: "4",
+		style: { stroke: "black" },
+	},
+];
+
+export default function App() {
+	const [nodes, , onNodesChange] = useNodesState(initialNodes);
+	const [edges, , onEdgesChange] = useEdgesState(initialEdges);
 
 	return (
-		<ReactFlow nodes={nodes} edges={edges} fitView>
-      <Background />
-      <Controls/>
+		<ReactFlow
+			nodes={nodes}
+			edges={edges}
+			onNodesChange={onNodesChange}
+			onEdgesChange={onEdgesChange}
+			fitView
+		>
+			<Background />
+			<Controls />
 		</ReactFlow>
 	);
 }
