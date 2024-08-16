a80b945fb73df4f3437a5b08a84c1566a56d09da
Edward Tanguay (windows) <edward@tanguay.info>
2024-08-16 11:47:12 +0200

    added nodes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import { ReactFlow } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
 export default function App() {
+
+  const nodes: Node[] = [];
+
   return (
-    <ReactFlow />
+    <ReactFlow nodes={nodes} />
   );
 }
