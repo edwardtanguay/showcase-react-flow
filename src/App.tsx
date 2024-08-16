81f42c8ffa709f865babef10846754cf1038dd09
Edward Tanguay (windows) <edward@tanguay.info>
2024-08-16 12:02:11 +0200

    grey boxes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import '@xyflow/react/dist/style.css';
 
 export default function App() {
 
-  const nodes: Node[] = [];
+  const nodes: Node[] = [{
+    id: '1',
+    data: {
+      label: 'Information'
+    },
+    position: { x: 0, y: 0 }
+  }];
 
   return (
     <ReactFlow nodes={nodes} />
