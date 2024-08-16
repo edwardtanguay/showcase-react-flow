701a644cb215802a8cc55720feb05f1ac03f8010
Edward Tanguay (windows) <edward@tanguay.info>
2024-08-16 15:16:43 +0200

    no workflow

--- src/Workflow/PaymentInit.tsx
+++
@@ -1,7 +0,0 @@
-// import { NodeProps } from "@xyflow/react";
-
-// export default function PaymentInit({
-// 	data: { amount },
-// }: NodeProps<{ amount: number }>) {
-// 	return <div>{amount}</div>;
-// }
