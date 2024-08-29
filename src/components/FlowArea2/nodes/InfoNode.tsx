45d26af2a6409dd162055d6cad20f9858a5bfd87
Edward Tanguay (windows) <edward@tanguay.info>
2024-08-29 16:10:22 +0200

    fixed shadows

--- src/components/FlowArea2/nodes/InfoNode.tsx
+++ src/components/FlowArea2/nodes/InfoNode.tsx
@@ -9,7 +9,7 @@ export function InfoNode({
 	const label = !data.label ? 'no text' : data.label;
 	return (
 		<div
-		style={{boxShadow: "1px 1px 10px 1px #555"}}
+		style={{boxShadow: "3px 3px 5px 0px #555"}}
 			className="p-2 border border-gray-400 flex justify-center items-center shadow-2xl h-[4rem] w-[4rem] bg-yellow-100"
 		>
 			<div className="text-xs" style={{color: textColor}}>{label}</div>
