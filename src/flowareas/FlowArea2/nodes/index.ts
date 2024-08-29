d4d9cc8d3851964f6d62ccf8b7568e3d707dcf8c
Edward Tanguay (windows) <edward@tanguay.info>
2024-08-29 19:11:42 +0200

    loading

--- src/flowareas/FlowArea2/nodes/index.ts
+++ src/flowareas/FlowArea2/nodes/index.ts
@@ -7,39 +7,14 @@ import * as tools from '../../../tools';
 export const initialNodes: AppNode[] = notes.map((note, index) => {
 	const x = 20 + (index * tools.getRandomNumberBetween(5,10));
 	const y = 20 + index * tools.getRandomNumberBetween(15, 30);
-	// const rotate = 20;
 	return {
 		id: `${index + 1}`,
-		data: { label: note, rotate: '25deg' },
+		data: { label: note, rotate: '28deg' },
 		type: "info-node",
 		position: { x, y },
 		height: 100,
 	};
 });
-
-// export const initialNodes: AppNode[] = [
-// 	{
-// 		id: "1",
-// 		data: { label: "Info 001" },
-// 		type: "info-node",
-// 		position: { x: 20, y: 20 },
-// 		height: 100,
-// 	},
-// 	{
-// 		id: "2",
-// 		data: {},
-// 		type: "info-node",
-// 		position: { x: 20, y: 100 },
-// 		height: 100,
-// 	},
-// 	{
-// 		id: "3",
-// 		data: { label: "Info 002" },
-// 		type: "info-node",
-// 		position: { x: 20, y: 180 },
-// 		height: 100,
-// 	},
-// ];
 
 export const nodeTypes = {
 	"info-node": InfoNode,
