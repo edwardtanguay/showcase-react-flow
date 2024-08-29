6dc88b7f36a92c281d5d042e6f5930890c1e1acf
Edward Tanguay (windows) <edward@tanguay.info>
2024-08-29 19:25:21 +0200

    more cards

--- src/flowareas/FlowArea2/nodes/InfoNode.tsx
+++ src/flowareas/FlowArea2/nodes/InfoNode.tsx
@@ -9,7 +9,7 @@ export function InfoNode({
 	const label = !data.label ? 'no text' : data.label;
 	return (
 		<div
-			style={{ boxShadow: "3px 3px 5px 0px #555", transform: `rotate(${data.rotate})` }}
+			style={{ boxShadow: "3px 3px 5px 0px #555", transform: `rotate(${data})` }}
 			className="p-2 border border-gray-400 flex justify-center items-center shadow-2xl h-[4rem] w-[4rem] bg-yellow-100"
 		>
 			<div className="text-xs" style={{ color: textColor }}>{label}</div>
