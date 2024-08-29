1042786b06ff4df71ec3869066f705902a7cf88a
Edward Tanguay (windows) <edward@tanguay.info>
2024-08-29 14:17:59 +0200

    no edges in 2

--- src/components/FlowArea2/index.tsx
+++ src/components/FlowArea2/index.tsx
@@ -1,42 +1,20 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import {
 	ReactFlow,
-	Edge,
 	Background,
 	Controls,
 	useNodesState,
 	useEdgesState,
-	addEdge,
-	Connection,
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
-import { useCallback } from "react";
 import { initialNodes, nodeTypes } from "../FlowArea1/nodes";
 import { initialEdges } from "../FlowArea1/edges";
 import { DividerLine } from "../FlowArea1/DividerLine";
 
 export const FlowArea2 = () => {
-	const [nodes, , onNodesChange] = useNodesState(initialNodes);
-	const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+	const [nodes] = useNodesState(initialNodes);
+	const [edges] = useEdgesState(initialEdges);
 
-	const onConnect = useCallback((connection: Connection) => {
-		const edge: Edge = {
-			...connection,
-			animated: false,
-			id: `${edges.length} + 1`,
-			label: "added",
-			style: {
-				stroke: "#222",
-			},
-			labelStyle: {
-				fill: "yellow",
-			},
-			labelBgStyle: {
-				fill: "black",
-			},
-		};
-		setEdges((prevEdges) => addEdge(edge, prevEdges));
-	}, []);
 	return (
 		<section>
 			<h2>React Flow 2</h2>
@@ -46,9 +24,6 @@ export const FlowArea2 = () => {
 					nodes={nodes}
 					nodeTypes={nodeTypes}
 					edges={edges}
-					onNodesChange={onNodesChange}
-					onEdgesChange={onEdgesChange}
-					onConnect={onConnect}
 				>
 					<Background />
 					<Controls showInteractive={false} />
