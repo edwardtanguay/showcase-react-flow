95d63d44b5740266e711f64580262165591b0ae6
Edward Tanguay (windows) <edward@tanguay.info>
2024-08-29 19:07:53 +0200

    working on rotate

--- src/flowareas/FlowArea2/nodes/types.ts
+++ src/flowareas/FlowArea2/nodes/types.ts
@@ -1,7 +1,8 @@
 import type { Node, BuiltInNode } from "@xyflow/react";
 
 type InfoNodeType = {
-	label?: string
+	label?: string,
+	rotate: string
 }
 
 export type InfoNode = Node<InfoNodeType, 'info-node'>;
---
+++ src/tools.ts
@@ -0,0 +1,3 @@
+export const getRandomNumberBetween = (start: number, end: number): number => {
+	return Math.floor(Math.random() * (end - start + 1)) + start;
+};
