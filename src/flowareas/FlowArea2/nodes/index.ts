027d7e573e5c17ca9fdb105c09b66bed190c60da
Edward Tanguay (windows) <edward@tanguay.info>
2024-08-30 09:13:38 +0200

    notes loading as one node

--- src/flowareas/FlowArea2/nodes/index.ts
+++ src/flowareas/FlowArea2/nodes/index.ts
@@ -5,20 +5,28 @@ import notes from "../data/notes.json";
 import * as tools from "../../../tools";
 import { EmployeeNode } from "./EmployeeNode";
 
-export const initialNodes: AppNode[] = notes.map((note, index) => {
-	const x = 5 * tools.getRandomNumberBetween(5, 20);
-	const y = 5 * tools.getRandomNumberBetween(5, 20);
-	const rotate = `${tools.getRandomNumberBetween(-20, 20)}deg`;
-	return {
-		id: `${index + 1}`,
-		data: { label: note, rotate },
-		type: "infoNode",
-		position: { x, y },
-		height: 100,
-	};
-});
+const getInitialNodes = (): AppNode[] => {
+	const nodes: AppNode[] = [];
+
+	notes.forEach((note, index) => {
+		const x = 5 * tools.getRandomNumberBetween(5, 20);
+		const y = 5 * tools.getRandomNumberBetween(5, 20);
+		const rotate = `${tools.getRandomNumberBetween(-20, 20)}deg`;
+		nodes.push({
+			id: `${index + 1}`,
+			data: { label: note, rotate },
+			type: "infoNode",
+			position: { x, y },
+			height: 100,
+		});
+	});
+
+	return nodes;
+};
+
+export const initialNodes: AppNode[] = getInitialNodes();
 
 export const nodeTypes: NodeTypes = {
 	infoNode: InfoNode,
-	employeeNode: EmployeeNode
+	employeeNode: EmployeeNode,
 };
