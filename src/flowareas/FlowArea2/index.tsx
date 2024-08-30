07eecadf59b8dcec697a56908b9d2645704a4e0c
Edward Tanguay (windows) <edward@tanguay.info>
2024-08-30 14:03:13 +0200

    small fixes

--- src/flowareas/FlowArea2/index.tsx
+++ src/flowareas/FlowArea2/index.tsx
@@ -11,6 +11,9 @@ import { initialNodes, nodeTypes } from "./nodes";
 import { initialEdges } from "./edges";
 import { FlowAreaWrapper } from "../FlowAreaWrapper";
 
+(async () => {
+	
+})();
 export const FlowArea2 = () => {
 	const [nodes, , onNodesChange] = useNodesState(initialNodes);
 	const [edges] = useEdgesState(initialEdges);
--- src/flowareas/FlowArea2/nodes/EmployeeNode.tsx
+++ src/flowareas/FlowArea2/nodes/EmployeeNode.tsx
@@ -6,7 +6,7 @@ export function EmployeeNode({
 }: NodeProps<EmployeeNode>) {
 
 	return (
-		<div className="bg-slate-200 pt-3 pl-3 pr-3 pb-1 border border-slate-400 rounded">
+		<div className="bg-slate-200 pt-3 pl-3 pr-3 pb-1  border border-slate-400 rounded">
 			<img className="w-[10rem] h-[10rem]" src={`images/employees/employee_${data.employee.id}.jpg`}/>
 			<div>{data.employee.firstName} {data.employee.lastName}</div>
 		</div>
